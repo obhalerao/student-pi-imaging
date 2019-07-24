36585e6ac99459ad89fa51b678e3979680a77a04
remotesensing <cameroninoz@gmail.com>
2019-07-24 08:53:03 -0400

    random message

--- time_lapse.py
+++ time_lapse.py
@@ -5,7 +5,7 @@ import math
 from picamera import PiCamera
 from time import sleep
 import time
-
+ 
 
 # Enter the path where the raspberry pi will store its file
 RASPI_PATH = ""
@@ -19,7 +19,7 @@ camera.start_preview()
 
 # Have the camera take pictures at the specified interval until the session is over using camera.capture(image.filename)
 for i in range(25):
-	camera.capture("IMG_{}".format(i))
+	camera.capture("IMG_{}".format(i)) 
 	time.sleep(2)
 
 camera.stop_preview()
