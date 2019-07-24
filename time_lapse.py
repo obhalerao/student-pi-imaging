5ae29d35a49220cbe96e794c9d0444260d7a2b95
cmastoras <cameroninoz@gmail.com>
2019-07-24 14:18:20 +0100

    did stuff

--- time_lapse.py
+++ time_lapse.py
@@ -1,11 +1,11 @@
-import os 
+import os
 import datetime
 from datetime import tzinfo
 import math
 from picamera import PiCamera
 from time import sleep
 import time
- 
+
 
 # Enter the path where the raspberry pi will store its file
 RASPI_PATH = "images_{}".format(time.asctime().replace(" ","_"))
@@ -14,6 +14,9 @@ os.mkdir(RASPI_PATH)
 
 INTERVAL = 2# the time interval (in seconds) between pictures
 SESSION_LENGTH = 600# the duration of the script
+START_TIME = 30 #the time that we have to get the code running before it starts taking pictures
+
+time.sleep(START_TIME)
 
 camera = PiCamera()
 camera.start_preview()
